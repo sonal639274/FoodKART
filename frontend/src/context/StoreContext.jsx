fd35ab943a4c4ab78effeeb9e69fa6ed1d68a84c
sonal639274 <164471196+sonal639274@users.noreply.github.com>
2025-04-10 18:39:19 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext =  createContext(null);
 const StoreContextProvider = (props)=>{
 
     const [cartItems,setCartItems]= useState({});
-    const url = 'http://localhost:4000';
+    const url = 'https://foodkart-backend-gp28.onrender.com';
     const [token, setToken] = useState("")
     const [food_list, setFood_list] = useState([])
 

650218d24d7931a37eece914c0f8820f3c74b969
sonal639274 <164471196+sonal639274@users.noreply.github.com>
2025-05-07 16:17:30 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,11 +2,16 @@ import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
+import Loader from '../Loader'
 
 const FoodDisplay = ({category}) => {
 
     const { food_list } = useContext(StoreContext)
-
+if(!food_list.length){
+        return(
+            <Loader/>
+        )
+    }
     return (
         <div className='food-display' id='food-display'>
             <h2>Top Dishes Near You</h2>
